e3fdfde2ba4ebcf7c5b55efbdb13ccd5a05f9d90
Mykola Dubich <98731880+Sm0kk@users.noreply.github.com>
2022-06-26 20:58:12 +0300

    Ready to push

---
+++ src/main/java/taxi/controller/driver/AddDriverController.java
@@ -0,0 +1,43 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.naming.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        String password = req.getParameter("password");
+        String repeatPassword = req.getParameter("repeat_password");
+        if (!password.equals(repeatPassword)) {
+            req.setAttribute("errorMsg",
+                    new AuthenticationException("Can't add driver, "
+                            + "check Password and Repeat password."));
+            req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+            return;
+        }
+        String name = req.getParameter("name");
+        String licenseNumber = req.getParameter("license_number");
+        String login = req.getParameter("login");
+        Driver driver = new Driver(name, licenseNumber, login, password);
+        driverService.create(driver);
+        resp.sendRedirect(req.getContextPath() + "/drivers/add");
+    }
+}
